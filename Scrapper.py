71fd2ceebbd32d259d06e1e589ba207031e7c729
Thomas Lu <66805615+ApparentlyAFK@users.noreply.github.com>
2023-05-21 11:13:03 -0700

    Fetch + Update card prices from TCGPlayer

--- Scrapper.py
+++ Scrapper.py
@@ -12,3 +12,22 @@ creds = Credentials.from_service_account_file('python-web-scrapper@web-scrapper-
 
 gc = gspread.authorize(creds)
 sheet = gc.open("your_google_sheet_name").sheet1
+
+
+# Fetch card prices from TCGPlayer
+url = 'https://shop.tcgplayer.com/price-guide/one-piece-card-game/romance-dawn'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+rows = soup.find_all('tr')
+
+for row in rows[1:]:
+    data = row.find_all('td')
+    card_name = data[0].text
+    sell_price = float(data[4].text.replace("$", ""))
+
+    # Round up card price
+    sell_price = math.ceil(sell_price / 0.25) * 0.25
+
+    # Find the card in the Google Sheet and update the sell price
+    cell = sheet.find(card_name)
+    sheet.update_cell(cell.row, 'column_number_of_sell_price', sell_price)

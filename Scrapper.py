2db79fba2960efab5f59afa2bc3a548d93efe2c9
Thomas Lu <66805615+ApparentlyAFK@users.noreply.github.com>
2023-05-21 11:09:29 -0700

    Setup Google Sheets API

--- Scrapper.py
+++ Scrapper.py
@@ -6,7 +6,9 @@ from google.auth.transport.requests import Request
 from google.oauth2.service_account import Credentials
 
 
-html_text = requests.get('...').text
-soup = BeautifulSoup(html_text, 'lxml')
-name = soup.find_all()
-price = soup.find_all()
+# Setup Google Sheets API
+scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+creds = Credentials.from_service_account_file('python-web-scrapper@web-scrapper-387417.iam.gserviceaccount.com', scopes=scope)
+
+gc = gspread.authorize(creds)
+sheet = gc.open("your_google_sheet_name").sheet1

3552b0f4039227288f0ec71b1ccc5c1028c76971
Thomas Lu <66805615+ApparentlyAFK@users.noreply.github.com>
2023-05-21 10:41:17 -0700

    Update imported libraries

--- Scrapper.py
+++ Scrapper.py
@@ -1,4 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
+import gspread
+import math
+from google.auth.transport.requests import Request
+from google.oauth2.service_account import Credentials
 
-html_text = requests.get('')
+
+html_text = requests.get('...').text
+soup = BeautifulSoup(html_text, 'lxml')
+name = soup.find_all()
+price = soup.find_all()
